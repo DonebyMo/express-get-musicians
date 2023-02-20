e748bd438b6573c20d826c80656356677aa401d0
Muhammad <muhammadalnabhani@gmail.com>
2023-02-20 15:56:28 +0000

    Get request for musicians

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
-const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const { Musician } = require("./Musician");
+const { sequelize } = require("./db");
 
 const port = 3000;
 
 //TODO
+app.get("/musicians", async (request, response) => {
+  const musicians = await Musician.findAll();
+  await response.json(musicians);
+});
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
-})
+  sequelize.sync();
+  console.log(`Listening on port ${port}`);
+});
